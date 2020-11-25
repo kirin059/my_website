5166ebc819e401780281ba20059dd0bcf094588a
kirin059 <71425369+kirin059@users.noreply.github.com>
2020-11-25 19:05:18 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const toggleBtn = document.querySelector('.navbar_toggleBtn');
 const menu = document.querySelector('.navbar_menu');
 const icon = document.querySelector('.navbar_icon');
 
-toggleBtn.addEventListner('click', function() {
+toggleBtn.addEventListener('click', function() {
     menu.classList.toggle('active');
     icon.classList.toggle('active');
     });
@@ -14,29 +14,29 @@ toggleBtn.addEventListner('click', function() {
 /* Section(1)_clock*/ 
 const clock=document.querySelector('.main-clock');
 
-      function getTime() {
-        const time = new Date();
-        const minutes = time.getMinutes();
-        const hours = time.getHours();
-        const seconds = time.getSeconds();
-        //clock.innerHTML = hour + ":" + minutes + ":" + seconds;
-        clock.innerHTML = `${                      /* 시간이 두자리로 나오게 하는 명령문 */
-          hours <10 ? `0${hours}` : hours
-          }
-          :${    
-          minutes < 10 ? `0${minutes}` : minutes
-          }
-          :${
-          seconds < 10 ? `0${seconds}` : seconds
-          }`
-      }
+    function getTime() {
+      const time = new Date();
+      const minutes = time.getMinutes();
+      const hours = time.getHours();
+      const seconds = time.getSeconds();
+      //clock.innerHTML = hour + ":" + minutes + ":" + seconds;
+      clock.innerHTML = `${                      /* 시간이 두자리로 나오게 하는 명령문 */
+        hours <10 ? `0${hours}` : hours
+        }
+        :${    
+        minutes < 10 ? `0${minutes}` : minutes
+        }
+        :${
+        seconds < 10 ? `0${seconds}` : seconds
+        }`
+    }
 
-      function init(){
-          getTime();
-          setInterval(getTime, 1000);
-      }
+    function init(){
+        getTime();
+        setInterval(getTime, 1000);
+    }
 
-      init();
+    init();
 /*
 ${지정된 함수를 불러오는 것}  예를들면,
 function color(red, blue){
@@ -53,107 +53,76 @@ hour가 10보다 작으면(if) `0을 hour앞에 붙여주고` 그렇지않으면
 
 /* Section(2)_img sliding */
 var slideIndex = 1;
-showSlides(slideIndex);
+        showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  var prev = document.getElementsByClassName('prev');
-  var next = document.getElementsByClassName('next');
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var dots = document.getElementsByClassName("dot");
+          var prev = document.getElementsByClassName('prev');
+          var next = document.getElementsByClassName('next');
 
-  if (n > slides.length) {
-    slideIndex = 1
-  }    
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000);
-}
-
-
-
-
-/* footer */
-
-/*
-function handler(self){
-    var target=document.querySelector('body')
-    if(self.value==='night') {
-      target.style.backgroundColor='black';
-      target.style.color='white';
-      document.querySelector('#click').value='day';
-          var alist=document.querySelectorAll('a');
-          var i=0;
-          while(i < alist.length){
-            alist[i].style.color='powderblue';
-            i=i+1;
+          if (n > slides.length) {
+            slideIndex = 1
+          }    
+          if (n < 1) {
+            slideIndex = slides.length
           }
-    } else {
-      target.style.backgroundColor='white';
-      target.style.color='black';
-      this.value='night';
-          var alist=document.querySelectorAll('a');
-          var i=0;
-          while(i < alist.length){
-            alist[i].style.color='blue';
-            i=i+1;
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
           }
-      }
-  }
-  */
+          for (i = 0; i < dots.length; i++) {
+              dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " active";
+          setTimeout(showSlides, 2000);
+}
 
 
- var Body = {
+//footer_dark mode
+var Body = {
   setBackColor : function(color) {
   document.querySelector('body').style.backgroundColor=color;
   },
   setColor : function(color) {
-    document.querySelector('body').style.color=color;
+  document.querySelector('body').style.color=color;
   }
 }
 
 var Link = {
   setColor : function (color){
-    var alist=document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
+  var alist=document.querySelectorAll('a');
+  var i = 0;
+  while(i < alist.length){
       alist[i].style.color=color;
       i=i+1;
-    }
+  }
   }
 }
 
 function handler(self){
-    var target=document.querySelector('body')
-    if(self.value==='night') {
+  var target=document.querySelector('body')
+  if(self.value==='night') {
       Body.setBackColor('black');
       Body.setColor('white');
       self.value='day';
 
-        Link.setColor('powderblue');
+      Link.setColor('powderblue');
 
-    } else {
+  } else {
       Body.setBackColor('white');
       Body.setColor('black');
       self.value='night';
 
-        Link.setColor('blue');
+      Link.setColor('blue');
       }
   }
